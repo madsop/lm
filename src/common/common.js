6538091951e4566e7dee263f6cd5906ee9df986a
'Mads Opheim' <devnull@localhost>
2013-09-08 23:07:46 +0200

    Linta ferdig

--- src/common/common.js
+++ src/common/common.js
@@ -1,27 +1,27 @@
-var LM = LM || {};
+var LM = this.LM || {};
 
-if (typeof require === "function" && typeof module !== "undefined") {
-  var _ = require("lodash");
+if (typeof require === "function" && module !== "undefined") {
+    var _ = require("lodash");
 }
 
 (function (namespace) {
-	namespace.flyttInnlegg = function (innlegg, opp, taleliste) { 
-		var innlegg = _.find(taleliste, function (element) { return innlegg.id == element.id; });
-	        var index = _.indexOf(taleliste, innlegg);
+    "use strict";
+    namespace.flyttInnlegg = function (innlegg2, opp, taleliste) {
+        var innlegg = _.find(taleliste, function (element) { return innlegg2.id === element.id; }),
+            index = _.indexOf(taleliste, innlegg);
 
-		if (opp){
-			if (index<1 || taleliste[index-1].type !== "Innlegg") { return; }
-			taleliste[index] = taleliste[index-1];
-			taleliste[index-1] = innlegg;
-		}
-		else {
-			if (index===taleliste.length-1 || taleliste[index].type !== "Innlegg") { return; }
-			taleliste[index] = taleliste[index+1];
-			taleliste[index+1] = innlegg;
-		}
-	};
+        if (opp) {
+            if (index < 1 || taleliste[index - 1].type !== "Innlegg") { return; }
+            taleliste[index] = taleliste[index - 1];
+            taleliste[index - 1] = innlegg;
+        } else {
+            if (index === taleliste.length - 1 || taleliste[index].type !== "Innlegg") { return; }
+            taleliste[index] = taleliste[index + 1];
+            taleliste[index + 1] = innlegg;
+        }
+    };
 }(LM));
 
-if (typeof require === "function" && typeof module !== "undefined") {
-	module.exports.flyttInnlegg = LM.flyttInnlegg;
+if (typeof require === "function" && module !== "undefined") {
+    module.exports.flyttInnlegg = LM.flyttInnlegg;
 }
