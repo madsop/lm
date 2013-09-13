354817f31606fd222da9004ce14e41643a7c1925
'Mads Opheim' <devnull@localhost>
2013-09-14 00:09:34 +0200

    Satte opp buster.js-rammeverket

--- test/buster.js
+++ test/buster.js
@@ -1,19 +1,15 @@
 config = module.exports;
 
-config["My tests"] = {
+config["Client tests"] = {
     rootPath: "../",
     environment: "browser",
-    libs: [
-	"lib/knockout-2.1.0.js"
-    ],
-    sources: [
-    ],
-    tests: [
-	"test/lm-test.js"
-    ]/*,
-    excludes: [
-            "jquery",
-            "knockout",
-            "sinon"
-    ]*/
-}
+    libs: ["lib/**/*.js"],
+    sources: ["src/*.js"],
+    tests: ["test/shared/**/*-test.js", "test/client/**/*-test.js"]
+};
+
+config["Server tests"] = {
+    rootPath: "../",
+    environment: "node",
+    tests: ["test/shared/**/*-test.js"]
+};
